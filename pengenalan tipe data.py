84b9b478eb8eb2c0bf55384553324bed154939f6
Arfan0306 <brarfan83@gmail.com>
2022-10-18 15:04:27 +0800

    days 2

---
+++ pengenalan tipe data.py
@@ -0,0 +1,22 @@
+a = "arfan muhadir"
+b = 19
+c = 1.9
+d = ["abc","19","1.9"]
+e = ("abc","19","1.9")
+f = {"nama":"arfan muhadir",'umur': 19}
+print (a) # ini adalah tipe data String
+print (b) # ini adalah tipe data Integer
+print (c) # ini adalah tipe data Float
+print (d) # ini adalah tipe data List yang meyimpan berbagai tipe data dan isinya bisa diubah-ubah
+print (e) # ini adalah tipe data Tuple yang meyimpan berbagai tipe data tapi isinya tidak bisa diubah-ubah
+print (f) # ini adalah tipe data Dictionary yang meyimpan berbagai tipe data berupa pasangan penunjuk dan nilai
+
+#Untuk mengecek jenis tipe data
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+
+
