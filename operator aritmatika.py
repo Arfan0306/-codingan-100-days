74a794fb1df93bb2d164daba9374ea0e6edec8a4
Arfan0306 <brarfan83@gmail.com>
2022-10-19 18:56:05 +0800

    days 3

---
+++ operator aritmatika.py
@@ -0,0 +1,23 @@
+x = int(input("masukan nilai x :")
+y = int(input("masukan nilai y :")
+
+#penjumlahan
+print("x + y = ", x+y)
+
+#pengurangan
+print("x - y = ", x-y)
+
+#perkalian
+print("x * y = ", x*y)
+
+#pembagian
+print("x / y = ", x/y)
+
+#sisabagi%modulus
+print("x % y = ", x%y)
+
+#pangkat
+print("x ** y = ", x**y)
+
+#pembagian bulat
+print("x + y = ", x//y)
