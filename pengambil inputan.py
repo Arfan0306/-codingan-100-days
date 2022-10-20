161505e61208f7fd65e5abbb56369e2ea5f9c63d
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-10-20 20:26:42 +0800

    days 4

---
+++ pengambil inputan.py
@@ -0,0 +1,9 @@
+nama = input("masukan nama anda :")
+nim = input("msukan nim anda :")
+prodi = input("masukan prodi anda :")
+fakultas = input("masukan fakultas anda :")
+
+print("NAMA     :",nama)
+print("NIM      :",nim)
+print("PRODI    :",prodi)
+print("FAKULTAS :",fakultas)
