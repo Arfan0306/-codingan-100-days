9b16ed089b4b991a7311500deb8525c95be35cda
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-10-21 20:21:10 +0800

    days 4

---
+++ mengambil inputan.py
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+nama = input("masukan nama anda :")
+nim = input("msukan nim anda :")
+prodi = input("masukan prodi anda :")
+fakultas = input("masukan fakultas anda :")
+
+print("NAMA     :",nama)
+print("NIM      :",nim)
+print("PRODI    :",prodi)
+print("FAKULTAS :",fakultas)
+=======
+nama = input("masukan nama anda :")
+nim = input("msukan nim anda :")
+prodi = input("masukan prodi anda :")
+fakultas = input("masukan fakultas anda :")
+
+print("NAMA     :",nama)
+print("NIM      :",nim)
+print("PRODI    :",prodi)
+print("FAKULTAS :",fakultas)
+>>>>>>> 161505e61208f7fd65e5abbb56369e2ea5f9c63d
