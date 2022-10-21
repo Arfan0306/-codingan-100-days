9d54fe00338046748e6a3368ce5716a329768089
Arfan0306 <brarfan83@gmail.com>
2022-10-21 19:59:03 +0800

    days 5

---
+++ percabangan.py
@@ -0,0 +1,9 @@
+bil1 = int(input("masukan bil1 :"))
+bil2 = int(input("masukan bil2 :"))
+
+if bil1>bil2:
+    hasil = " bilangan 1 lebih besar dari bilangan 2"
+    print(hasil)
+else:
+    hasil = "bilangan satu lebih kecil dari bilangan 2"
+    print(hasil)
