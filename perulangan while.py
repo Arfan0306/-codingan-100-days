558dda325e426ebb34a5fd316276a80d24e396a2
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-10-24 23:21:29 +0800

    days 8

---
+++ perulangan while.py
@@ -0,0 +1,6 @@
+
+#perulangan while suatu pengulangan yang akan dilakukan terus-menerus selama suatu kondisi terpenuhi
+angka = int(input("masukan angka :"))
+while angka <= 10:
+    print (angka)
+    angka += 1
