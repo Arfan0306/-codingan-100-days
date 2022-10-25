b507ebbf2aa3886fb704643ff63df343998328ea
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-10-25 21:51:25 +0800

    days 9

---
+++ contoh kasus logika.py
@@ -0,0 +1,10 @@
+warna = input("masukan warna lampu : ")
+
+if warna == "merah":
+    print("BERHENTI")
+elif warna == "orange":
+    print("HATI-HATI")
+elif warna == "hijau":
+    print("SILAHKAN JALAN")
+else:
+    print("LAMPU YANG ANDA MASUKAN TIDAK ADA")
