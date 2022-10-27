be46d9cd5aacac406c06442ebf26c31704548998
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-10-27 21:37:51 +0800

    days 11

---
+++ contoh kasus login.py
@@ -0,0 +1,7 @@
+nama=input("masukan username :")
+pasword =int(input("masukan pasword :"))
+
+if nama == "arfan muhadir" and pasword == 2003:
+    print("LOGIN BERHASIL")
+else:
+    print("LOGIN GAGAL")
