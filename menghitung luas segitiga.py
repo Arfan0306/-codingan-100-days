fbb1fd500e1d29107ed974ae003b37012353a203
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-10-28 21:17:28 +0800

    days 12

---
+++ menghitung luas segitiga.py
@@ -0,0 +1,8 @@
+alas = float(input("Masukan Alas Segitiga: "))
+tinggi = float(input("Masusukan Tinggi Segitiga: "))
+ 
+# Hitung Luas Segitiga
+luas = (alas * tinggi) / 2
+ 
+#Menampilkan Hasil Perhitungan
+print("Luas Segitiga adalah :",luas)
