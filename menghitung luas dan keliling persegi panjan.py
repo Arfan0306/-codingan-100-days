2b1d910ea27955a453eb6c10466d6440f0c65a02
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-10-29 12:49:11 +0800

    days 13

---
+++ menghitung luas dan keliling persegi panjan.py
@@ -0,0 +1,9 @@
+#Menghitung luas dan keliling persegi panjang
+panjang = float(input("Masukan Panjang: "))
+lebar = float(input("Masukan Lebar: "))
+
+luas = panjang*lebar
+keliling = 2 * (panjang+lebar)
+
+print("Luas Persegi Panjang :",luas)
+print("Keliling Persegi Panjang :",keliling)
