755ae04eb8869fc91a2dfcae4fd03a7414d1b26f
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-10-30 22:03:40 +0800

    days 14

---
+++ tabel perkalian.py
@@ -0,0 +1,8 @@
+angka = int(input("masukan perkalian dari: "))
+ 
+# Menghitung 10 Kali dari Kisaran 1 sampai 10
+for a in range(1, 11):
+ 
+#Menampilkan Perkalian
+    hasil= angka*a
+    print(angka, "x", a, "=",hasil)
