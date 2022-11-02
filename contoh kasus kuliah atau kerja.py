dee2ef31687222163f1defd60f78a05fed122d56
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-11-02 20:17:30 +0800

    days 17

---
+++ contoh kasus kuliah atau kerja.py
@@ -0,0 +1,21 @@
+print("rencana setelah lulus sekolah \n1. kuliah \n2. kerja")
+pilihan = int(input("masukan pilihan :"))
+if pilihan == 1:
+    print("Mau kuliah jurusan apa \n1.Teknik Informatika \n2.teknik Sipil \n3.Perencanaan wilayah dan kota")
+    pilihan1 =int(input("masukan pilihan :"))
+    if pilihan1 == 1:
+        print("Teknik Informatika")
+    elif pilihan1 == 2:
+        print("Teknik Sipil")
+    else:
+        print("Perencanaan Wilayah dan Kota")
+else:
+    print("Mau kerja Apa \n1.pegawai bank \n2.operator alat berat \n3.bengkel")
+    pilihan2 =int(input("masukan pilihan :"))
+    if pilihan2 == 1:
+        print("Pegawai bank")
+    elif pilihan2 == 2:
+        print("Pegawai bank")
+    else:
+        print("bengkel")
+    
