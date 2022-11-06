bcc0e840cc1e508fadf3865fc7076ff167b2f70f
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-11-06 21:05:29 +0800

    days 21

---
+++ contoh_kasus_aritmatika.py
@@ -0,0 +1,10 @@
+komputer_sultan= 55000000
+gaji_pokok= 5000000
+uang_kos= 750000
+tabungan= 1500000
+hasil= komputer_sultan / tabungan
+print("Harga Komputer Sultan adalah :Rp.",komputer_sultan)
+print("Gaji pokok Amrillah Adalah :Rp.",gaji_pokok)
+print("Uang kos Amrillah adalah :Rp.",uang_kos)
+print("Tabungan Amrillah adalah :Rp.",tabungan)
+print("Lama Waktu yang Diperlukan Untuk membeli Komputer Sultan Adalah :",int(hasil),"Hari")
