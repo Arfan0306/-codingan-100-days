9b93c231cad272abd3e1d8ac639b730fda28b623
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-11-11 20:21:28 +0800

    days 26

---
+++ membuat_segiempat_bintang.py
@@ -0,0 +1,9 @@
+tinggi_persegi = int(input("masukan tinggi persegi :"))
+lebar_persegi = int(input("masukan lebar persegi :"))
+
+print()
+
+for i in range(tinggi_persegi):
+  for j in range(lebar_persegi):
+    print(" *",end="")
+  print()
