06a559aedd2c9f29b6bc9dbca62d9e141106cd84
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-11-13 22:22:46 +0800

    days 28

---
+++ operator_logika.py
@@ -0,0 +1,7 @@
+nilai1 =int(input("masukan nilai 1:"))
+nilai2 =int(input("masukan nilai 2:"))
+
+if nilai1 > nilai2:
+    print("nilai 1 lebih besar dari nilai 2")
+else:
+    print("nilai 1 lebih kecil dari nilai 2")
