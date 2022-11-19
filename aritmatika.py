d0055302e9b722c92cfefc1211b638199e638941
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-11-19 20:40:03 +0800

    days 34

---
+++ aritmatika.py
@@ -0,0 +1,22 @@
+# cara pertama
+import belaja
+
+belaja.tambah(4,8)
+belaja.kurang(9,2)
+
+# cara kedua
+import belaja as aritmatika
+
+aritmatika.tambah(4,8)
+aritmatika.kurang(9,2)
+
+#cara ketiga itu ada dua cara untuk menampilkan
+#cara pertama menampilkan
+from belaja import tambah, kurang
+
+#cara kedua menampilkan
+from belaja import *
+
+tambah(4,8)
+kurang(9,2)
+
