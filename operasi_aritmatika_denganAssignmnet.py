d41844150af4e7984246b81eebd2a3b9aba7be6e
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-11-29 21:40:32 +0800

    days 44

---
+++ operasi_aritmatika_denganAssignmnet.py
@@ -0,0 +1,18 @@
+# operasi yang dapat di lakukan dengan penyingkatan
+# operasi di tambah dengan assignment
+
+a = 5 # adalah assignment
+print("nilai a =",a)
+
+a += 1 # artinya adalah a = a + 1
+print("nilai a += 1 , nilai a menjadi :",a)
+
+a -= 2 # artinya adalah a = a - 2
+print("nilai a -= 2 , nilai a menjadi :",a)
+
+a *= 5 # artinya adalah a = a * 5
+print("nilai a *= 5 , nilai a menjadi :",a)
+
+a /= 2 # artinya adalah a = a / 2
+print("nilai a /= 2 , nilai a menjadi :",a)
+
