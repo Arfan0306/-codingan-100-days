68ff614a94032a5dda8e2b18256f47108c2c009a
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-11-30 20:24:13 +0800

    days 46

---
+++ operator_AND_denganAssignment.py
@@ -0,0 +1,13 @@
+# operasi yang dapat di lakukan dengan penyingkatan
+# operasi di tambah dengan assignment
+
+# AND
+c = True
+print("\nnilai c =",c)
+c &= False
+print("nilai c &= False , nilai c menjadi :",c)
+
+c = True
+print("\nnilai c =",c)
+c &= True
+print("nilai c &= True , nilai c menjadi :",c)
