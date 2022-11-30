2c081d3007c44f169cd74812135d5481c3b70577
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-11-30 20:33:43 +0800

    days 51

---
+++ pengenalan_String_menggunakan_tanda_backslash.py
@@ -0,0 +1,20 @@
+# menggunakan tanda backslash
+
+# membuat tanda ' menjadi string
+
+print('mari shalat jum\'at')
+print('g\'day, isn\'t it ?')
+
+# backlash
+print("C : \\user\\Ucup")
+
+# tab
+print("andi \tdian, semakin jauh")
+
+# backspace
+print("andi \bdian, jadi dekat")
+
+#newline
+print("baris pertama. \nbaris kedua.") # LF -> line feed -> unix, macos, linux
+print("baris pertama. \rbaris kedua.") # CR -> carriage return -> commodore, acorn, lisp
+print("baris pertama. \r\nbaris kedua.") # CRLF -> line feed carriage return -> dipakai oleh windows
