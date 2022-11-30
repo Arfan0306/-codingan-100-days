3bc09621758dfe6c26d3528d751a85f134a29250
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-11-30 20:29:20 +0800

    days 49

---
+++ operator_XOR_denganAssignment.py
@@ -0,0 +1,13 @@
+# operasi yang dapat di lakukan dengan penyingkatan
+# operasi di tambah dengan assignment
+
+# XOR
+c = True
+print("\nnilai c =",c)
+c ^= False
+print("nilai c ^= False , nilai c menjadi :",c)
+
+c = False
+print("\nnilai c =",c)
+c ^= False
+print("nilai c ^= False , nilai c menjadi :",c)
