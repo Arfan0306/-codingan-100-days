9f574f9df545ddbd43bcbe731edbd9ef442cd585
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-11-30 20:30:56 +0800

    days 50

---
+++ pengenalan_string.py
@@ -0,0 +1,29 @@
+data = "ini adalah string"
+print(data)
+print(type(data))
+
+# cara membuat string
+
+'''
+    1. dengan menggunakan sigle quote '...'
+    2. dengan menggunakan double quote "..."
+'''
+
+data = 'menggunakan sigle quote'
+print(data)
+
+data = "menggunakan double quote"
+print(data)
+
+print('"apa kabar?"')
+print("'apa kabar?'")
+print("ini adalah hari jum'at")
+
+
+
+
+
+
+
+
+
