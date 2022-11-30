ba6372eeb34919dd91f2e06d960a161ecb4f3740
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-11-30 20:28:20 +0800

    days 48

---
+++ operator_geser-geser_denganAssignment.py
@@ -0,0 +1,12 @@
+# operasi yang dapat di lakukan dengan penyingkatan
+# operasi di tambah dengan assignment
+
+# geser geser
+d = 0b0100
+print("\nnilai d =", format(d,'04b'))
+d >>= 2
+print("nilai d >> 2 , nilai D menjadi :",format(d,'04b'))
+d <<= 1
+print("nilai d << 1 , nilai D menjadi :",format(d,'04b'))
+
+
