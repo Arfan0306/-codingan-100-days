d091ec0ca9564e703d53506f0dbd52be9c337d5b
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-11-30 20:20:07 +0800

    days 45

---
+++ operasi_modulus_denganAssignment.py
@@ -0,0 +1,12 @@
+# operasi yang dapat di lakukan dengan penyingkatan
+# operasi di tambah dengan assignment
+
+# modulus dan floor division
+b = 10 # adalah assignment
+print("\nnilai b =",b)
+
+b //= 3
+print("nilai b //= 3 , nilai b menjadi :",b)
+
+a = 5
+print("\nnilai a =",a)
