f8e3a425dee4345186ab98b66495a3f55df52078
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-11-30 20:27:10 +0800

    days 47

---
+++ operator_bitwise_denganAssignment.py
@@ -0,0 +1,14 @@
+# operasi yang dapat di lakukan dengan penyingkatan
+# operasi di tambah dengan assignment
+
+
+# operator bitwise
+c = True
+print("\nnilai c =",c)
+c |= False
+print("nilai c |= False , nilai c menjadi :",c)
+
+c = False
+print("\nnilai c =",c)
+c |= False
+print("nilai c |= False , nilai c menjadi :",c)
