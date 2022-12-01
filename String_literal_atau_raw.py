2434ad1ee437692e53118fff530fba05e6fbeb47
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-12-01 22:39:22 +0800

    days 52

---
+++ String_literal_atau_raw.py
@@ -0,0 +1,19 @@
+# string literal atau raw
+# hati-hati
+print('C :\new folder') # akan salah
+
+# menggunakan raw String
+print(r'C :\new folder')
+
+# multiline literal String
+print("""
+nama : andi
+kls : 2 SMP
+""")
+
+# multiline literal string RAW
+print(r"""
+nama : andi
+kls : 2 SMP\new normal
+website : www.andi.com/newID
+""")
