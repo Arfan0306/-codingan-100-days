e7b65a90014700368327c163dc0ca64261be0fc7
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-12-09 01:38:20 +0800

    days 58

---
+++ else_if_statement.py
@@ -0,0 +1,14 @@
+# ELIF = else if statement
+
+nama = input("nama kamu siapa? ")
+
+if nama == "arfan": # kondisi 1
+    print("hai apa kabar") # aksi true 1
+
+elif nama == "kaco": # kondisi 2
+    print("hai kaco apa kabar") # aksi true 2
+elif nama == "cicci": # kondisi ke 3
+    print("hai cicci apa kabar") # aksi true 3
+else:
+    print("kamu salah masuk nama!") # aksi false
+print("terimakasi")
