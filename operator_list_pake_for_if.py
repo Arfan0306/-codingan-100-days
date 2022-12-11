c2166c41a7716c267db63e0f3020286575ea69f9
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-12-11 09:49:49 +0800

    days 62

---
+++ operator_list_pake_for_if.py
@@ -0,0 +1,9 @@
+# membuat list pake for pake if
+list_pake_for_if = [i for i in range (0,10) if i != 5]
+print(list_pake_for_if)
+
+list_pake_for_if = [i for i in range (0,10) if i i%2 ==0]
+print(list_pake_for_if)
+
+list_pake_for_if = [i for i in range (0,10) if i i%2 !=5]
+print(list_pake_for_if)
