0d7baaf166865621e72c74a65fd4df5462acef78
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-12-12 22:28:17 +0800

    days 63

---
+++ operasi_list.py
@@ -0,0 +1,11 @@
+data_angka = [1,2,5,3,5,2,5,6,7,8,9,0]
+
+print(f"data angka = \n{data_angka}")
+
+# count data
+
+jumlah_data_5 = data_angka.count(5)
+jumlah_data_2 = data_angka.count(2)
+
+print(f"jumlah angka 5 = {jumlah_data_5}")
+print(f"jumlah angka 2 = {jumlah_data_2}")
