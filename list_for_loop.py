b9ced30bad840126641ca5c8226c1df1e0b6b45e
ARFAN MUHADIR <99815861+Arfan0306@users.noreply.github.com>
2022-12-18 02:34:06 +0800

    days 67

---
+++ list_for_loop.py
@@ -0,0 +1,13 @@
+
+
+#for loop
+kumpulan_angka = [4,3,2,5,6,1]
+
+for angka in kumpulan_angka:
+    print(f"angka = {angka}")
+
+peserta = ["arfan","reski","muh","aprieni"]
+
+for nama in peserta:
+    print(f"nama = {nama}")
+
